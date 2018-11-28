0f887e701abfab597df03093098cf591089b304c
vonovo123 <vonovo123@naver.com>
2018-11-28 17:19:46 +0900

    projectMapper수정

--- src/main/java/com/keb/atic/project/mapper/ProjectMapper.java
+++ src/main/java/com/keb/atic/project/mapper/ProjectMapper.java
@@ -1,6 +1,7 @@
 package com.keb.atic.project.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import com.keb.atic.project.domain.Project;
 
@@ -16,13 +17,15 @@ public interface ProjectMapper {
 	// 조건별 프로젝트 조회(condition key: 목표 금액 순)
 	public List<Project> readProjectsByGoal();
 	// 조건별  등록 예정 프로젝트 조회(condition key: 목표 금액 순);
-	public List<Project> readMonthProjectByGoal(String month);
+	public List<Project> readMonthProjectByGoal(Map<String, String> map);
 	// 조건별 프로젝트 조회(condition key: 달성률 순)
 	public List<Project> readProjectsByPercent();
 	// 조건별 프로젝트 조회(condition key: 매력도 순)
 	public List<Project> readProjectsByEval();
 	// 전체 프로젝트 조회
 	public List<Project> projectListAll();
+	// 조건부 프로젝트 조회
+	public List<Project> readProjectByCondition(Map<String, String> map);
 	//프로젝트 삭제
 	public void deleteProject(String id);
 	//사용자 정보 업데이트
